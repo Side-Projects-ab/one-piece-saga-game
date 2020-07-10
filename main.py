149a31c8316a47e8b0372726c57dc21051193032
zceeaba <abhishekh.baskaran@thoughtriver.com>
2020-07-10 23:16:00 +0100

    Added functionality for a ground and also for the players to move up and down

--- main.py
+++ main.py
@@ -8,21 +8,16 @@ https://www.pygame.org/docs/ref/image.html#pygame.image.load
 """
 
 
-def draw_character_movement(screen: pygame.Surface, character: pygame.Surface):
-    x, y = pygame.mouse.get_pos()
-    x -= character.get_width() / 2
-    y -= character.get_height() / 2
-    screen.blit(character, (x, y))
-
-
 def create_character(file_path: str,
-                     character_ratio: float,
+                     width_ratio: float,
+                     height_ratio: float,
                      background: pygame.Surface) -> pygame.Surface:
     """
     Creates a character and do the necessary transformations.
-    :param background:
     :param file_path:
-    :param character_ratio:
+    :param width_ratio:
+    :param height_ratio:
+    :param background:
     :return:
     """
     # Add a level of transparency to the character
@@ -30,16 +25,47 @@ def create_character(file_path: str,
 
     # Transform the character to the appropriate size
     character = pygame.transform.scale(character,
-                                       (int(character_ratio * background.get_width()),
-                                        int(character_ratio * background.get_height())))
+                                       (int(width_ratio * background.get_width()),
+                                        int(height_ratio * background.get_height())))
 
     return character
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, surface_object, keys, speed, boundaries = (0, 0)):
+        super(Player, self).__init__()
+        self.surf = surface_object
+        self.rect = self.surf.get_rect()
+        self.up = keys["Up"]
+        self.down = keys["Down"]
+        self.right = keys["Right"]
+        self.left = keys["Left"]
+        self.boundaries = boundaries
+        self.speed = speed
+
+    def update(self, pressed_keys):
+        if pressed_keys[self.up]:
+            self.rect.move_ip(0, -1 * self.speed)
+        if pressed_keys[self.down]:
+            self.rect.move_ip(0, self.speed)
+        if pressed_keys[self.left]:
+            self.rect.move_ip(-1 * self.speed, 0)
+        if pressed_keys[self.right]:
+            self.rect.move_ip(self.speed, 0)
+        # If it is in the middle look in the other direction.
+        if self.rect.x == self.boundaries[0] / 2:
+            self.surf = pygame.transform.flip(self.surf, True, False)
+
+
+def draw_character(screen: pygame.Surface, character: Player):
+    screen.blit(character.surf, character.rect)
+
 
 def main():
     # Initialize image properties
     background_image = "background_level_1.jpg"
-    main_character_image = "luffy_character.png"
-    side_character_image = "zoro_character.jpg"
+    main_character_image = "Luffy2_character.png"
+    side_character_image = "zoro_character.png"
     ground_image = "ground.png"
 
     # Initialize all imported pygame modules
@@ -57,49 +83,84 @@ def main():
     background = pygame.image.load(background_image).convert()
 
     # Add the character ratio
-    ratio = 0.1
+    ratio_char = 0.1
+
+    # Add the ground ratio
+    ratio_ground_w = 0.8
+    ratio_ground_h = 0.1
 
     main_character = create_character(
         file_path = main_character_image,
-        character_ratio = ratio,
+        width_ratio = ratio_char,
+        height_ratio = ratio_char,
         background = background
     )
 
     second_character = create_character(
         file_path = side_character_image,
-        character_ratio = ratio,
+        width_ratio = ratio_char,
+        height_ratio = ratio_char,
         background = background
     )
 
     ground_obj = create_character(
         file_path = ground_image,
-        character_ratio = 1.0,
+        width_ratio = ratio_ground_w,
+        height_ratio = ratio_ground_h,
         background = background
     )
 
-    ground = {"width": 0, "height": screen.get_height()-20}
-
-    # Draws the background on the screen
-    screen.blit(background, (0, 0))
-
-    screen.blit(
-        main_character,
-        (0, (screen.get_height() - main_character.get_height()))
+    main_player = Player(
+        surface_object = main_character,
+        keys = {"Up": K_UP, "Down": K_DOWN, "Left": K_LEFT, "Right": K_RIGHT},
+        speed = 20,
+        boundaries = (screen.get_width(), screen.get_height())
     )
 
-    screen.blit(
-        second_character,
-        (screen.get_width() - second_character.get_width(),
-         (screen.get_height() - second_character.get_height()))
+    side_player = Player(
+        surface_object = second_character,
+        keys = {"Up": K_w, "Down": K_s, "Left": K_a, "Right": K_d},
+        speed = 20,
+        boundaries = (screen.get_width(), screen.get_height())
     )
+
+    ground = {"width": screen.get_width() / 8,
+              "height": screen.get_height() - ground_obj.get_height()}
+
+    main_player.rect.x, main_player.rect.y = ground["width"], \
+                                             (
+                                                     screen.get_height() - ground_obj.get_height() -
+                                                     main_character.get_height()
+                                             )
+    side_player.rect.x, side_player.rect.y = (ground["width"] + ground_obj.get_width() -
+                                              second_character.get_width()), \
+                                             (
+                                                     screen.get_height() - ground_obj.get_height() -
+                                                     second_character.get_height()
+                                             )
+
+    running = True
 
     # Infinite loop
-    while True:
+    while running:
 
         for event in pygame.event.get():
             if event.type == QUIT:
+                running = False
                 pygame.quit()
                 exit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+            pressed_keys = pygame.key.get_pressed()
+
+            # Update the main player
+            main_player.update(pressed_keys)
+
+            # Update the second player
+            side_player.update(pressed_keys)
 
             # Draws the background on the screen
             screen.blit(background, (0, 0))
@@ -108,10 +169,10 @@ def main():
             screen.blit(ground_obj, (ground["width"], ground["height"]))
 
             # Move main character
-            # draw_character_movement(screen = screen, character = main_character)
+            draw_character(screen = screen, character = main_player)
 
             # Move side character
-            # draw_character_movement(screen = screen, character = second_character)
+            draw_character(screen = screen, character = side_player)
 
             pygame.display.update()
 
